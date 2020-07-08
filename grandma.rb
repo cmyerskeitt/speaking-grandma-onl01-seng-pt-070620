183102eabec07bf9d51ed1a65fece8a3a70e8f7f
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-08 04:10:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase) 
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  eslif phrase == "Hi Nana, how are you?"
-    return 
-
-  
+  elsif phrase == "Hi Nana, how are you?"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
   else phrase == phrase.upcase
