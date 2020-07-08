0fc58382783be74fe7179ce0cbf8d41792a0467f
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-08 03:11:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,10 @@
 def speak_to_grandma(phrase) 
-if 
-  
-  phrase == phrase.downcase
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif phrase == phrase.upcase
-  puts "NO, NOT SINCE 1938!"
-else phrase == "I LOVE YOU GRANDMA!"
+if phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
+elsif phrase == phrase.downcase
+  puts "HUH?! SPEAK UP, SONNY!"
+else phrase == phrase.upcase
+  puts "NO, NOT SINCE 1938!"
 end
 
 # Whatever you say to grandma, she should respond with
