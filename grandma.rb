30f02d6cf7ebeb3de6d4b37c1908a7054b5456ef
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-08 03:11:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,7 @@
 def speak_to_grandma(phrase) 
-if phrase == phrase.downcase
+if 
+  
+  phrase == phrase.downcase
   puts "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.upcase
   puts "NO, NOT SINCE 1938!"
